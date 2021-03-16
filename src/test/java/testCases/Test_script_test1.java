d9b95ae7429422b64c37b0c46e83e8db6a36e91b
shubham <kumarshubham2994@gmail.com>
2021-03-16 17:10:37 +0530

    fifth change

--- src/test/java/testCases/Test_script_test1.java
+++ src/test/java/testCases/Test_script_test1.java
@@ -73,8 +73,7 @@ public class Test_script_test1 extends baseTest{
 		System.out.println("GIT TRIAL");
 		System.out.println("GIT TRIAL");
 		System.out.println("GIT TRIAL");
-		System.out.println("GIT TRIAL");
-		System.out.println("GIT TRIAL");
+
 	}
 
 	@AfterMethod
