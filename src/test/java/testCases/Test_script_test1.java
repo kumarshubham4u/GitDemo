980b095f3be888799fd4078eb110ed8e59f9f4da
shubham <kumarshubham2994@gmail.com>
2021-03-17 12:57:27 +0530

    newwww

--- src/test/java/testCases/Test_script_test1.java
+++ src/test/java/testCases/Test_script_test1.java
@@ -83,6 +83,13 @@ public class Test_script_test1 extends baseTest{
 		System.out.println("GIT TRIAL");
 		System.out.println("GIT TRIAL");
 		System.out.println("GIT TRIAL");
+		
+		System.out.println("GIT TRIAL");
+		System.out.println("GIT TRIAL");
+		System.out.println("GIT TRIAL");
+		System.out.println("GIT TRIAL");
+		System.out.println("GIT TRIAL");
+		System.out.println("GIT TRIAL");
 	}
 
 	@AfterMethod
